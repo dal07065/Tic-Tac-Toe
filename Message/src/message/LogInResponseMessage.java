715f4a5127a4cd98d6ac0bb74708ec55e13003f6
Lina <dal07065@gmail.com>
2021-05-27 13:11:15 -0700

    Update LogInResponseMessage.java

--- Message/src/message/LogInResponseMessage.java
+++ Message/src/message/LogInResponseMessage.java
@@ -3,7 +3,8 @@ package message;
 public class LogInResponseMessage extends UserInfoMessage{
 
     private boolean success;
-    public LogInResponseMessage(boolean success, String userID, String password, String firstName, String lastName) {
+    public LogInResponseMessage(boolean success, String userID, String password, String firstName, String lastName,
+                                int wins, int losses, int ties) {
         super(userID, password, firstName, lastName);
         this.success = success;
     }
